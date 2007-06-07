529e3f6b1b9cc94293ce9e46fcaaacdfddbc9c2a
Flemming Bregnvig <flemming@bregnvig.dk>
2007-06-07 21:18:55 +0000

    Sharing
    git-svn-id: https://svn.iportalen.dk/svn-private/F2007/trunk@3 d9c49cb3-4128-44d0-8390-2fdd9ac6ca64

---
+++ core/src/main/java/dk/bregnvig/formula1/model/Season.java
@@ -0,0 +1,89 @@
+package dk.bregnvig.formula1.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * This class is a entire season of formula 1
+ * @author bregnvig
+ *
+ */
+@Entity
+@Table(name="season")
+public class Season {
+	
+	private Long id;
+	
+	private String name;
+	private Set<Race> races = new HashSet<Race>(21);
+	private Set<Player> players = new HashSet<Player>(17);
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Column(length=50, nullable=false, unique=true)
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Adds a single player to the season. The player must already be persisted
+	 * @param player
+	 */
+	public void addPlayer(Player player) {
+		players.add(player);
+	}
+	
+	@ManyToMany(fetch=FetchType.EAGER, cascade= {CascadeType.MERGE, CascadeType.REFRESH})
+	@JoinTable(name="season_player")
+	public Set<Player> getPlayers() {
+		return players;
+	}
+	public void setPlayers(Set<Player> players) {
+		this.players = players;
+	}
+	
+	public void removePlayer(Player player) {
+		players.remove(player);
+	}
+	
+	public void addRace(Race race) {
+		races.add(race);
+	}
+	
+	@OneToMany(cascade=CascadeType.ALL)
+	@JoinColumn(name="season_id")
+	public Set<Race> getRaces() {
+		return races;
+	}
+	public void setRaces(Set<Race> races) {
+		this.races = races;
+	}
+	
+	
+	
+}
