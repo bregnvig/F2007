f4190a8c513fefa15d9009c3ab1e6d90f8ed1c92
Flemming Bregnvig <flemming@bregnvig.dk>
2007-06-19 20:38:56 +0000

    Version 1.0 Simple version.
    git-svn-id: https://svn.iportalen.dk/svn-private/F2007/trunk@233 d9c49cb3-4128-44d0-8390-2fdd9ac6ca64

--- core/src/test/java/dk/bregnvig/formula1/RaceTest.java
+++ core/src/test/java/dk/bregnvig/formula1/RaceTest.java
@@ -1,35 +1,15 @@
 package dk.bregnvig.formula1;
 
 import java.util.Calendar;
+import java.util.List;
 
-import dk.bregnvig.formula1.Race;
-import dk.bregnvig.formula1.Season;
 import dk.bregnvig.formula1.util.AbstractDaoTest;
 
 public class RaceTest extends AbstractDaoTest {
 	
-	private Race race = new Race();
-	
-	private Season season;
-
 	@Override
 	protected void onSetUpInTransaction() throws Exception {
 		super.onSetUpInTransaction();
-		race.setSelectedDriver(kimi);
-		race.setName("Monaco");
-		race.setBegin(Calendar.getInstance());
-		Calendar close = Calendar.getInstance();
-		close.add(Calendar.MONTH, 1);
-		race.setClose(close);
-		
-		season = new Season();
-		season.setName("F1 2007");
-		season.addPlayer(flb);
-		season.addPlayer(mba);
-		season.addPlayer(ttp);
-		season.addRace(race);
-		getEntityManager().persist(season);
-		
 	}
 	
 	public void testEntityManagerThere() {
@@ -37,36 +17,106 @@ public class RaceTest extends AbstractDaoTest {
 	}
 	
 	public void testRaceCreated() {
-		assertNotNull(race.getId());
-		assertNotNull(race.getSeason());
+		assertNotNull(monza.getId());
+		assertNotNull(monza.getSeason());
 	}
 
 	public void testRaceFind() {
-		assertNotNull(getEntityManager().find(Race.class, race.getId()));
+		assertNotNull(getEntityManager().find(Race.class, monza.getId()));
 	}
 	
 	public void testDeleteRace() {
-		getEntityManager().remove(race);
-		assertNull(getEntityManager().find(Race.class, race.getId()));
+		getEntityManager().remove(monza);
+		assertNull(getEntityManager().find(Race.class, monza.getId()));
 	}
 	
+	public void testAddBid() {
+		Bid bid = helper.getBid(flb, kimi, massa, hamilton, alonso, heidfeld, kubica, 1, 1, 100000);
+		monza.addBid(bid);
+		getEntityManager().flush();
+		assertNotNull(bid.getId());
+	}
+
+	public void testAddBidForNonPlayer() {
+		
+		Player player = new Player();
+		player.setFirstName("Fie");
+		player.setLastName("Pedersen");
+		player.setPlayername("fie");
+		getEntityManager().persist(player);
+		
+		assertNotNull(player.getId());
+		
+		Bid bid = helper.getBid(player, kimi, massa, hamilton, alonso, heidfeld, kubica, 1, 1, 100000);
+		try {
+			monza.addBid(bid);
+			fail("Player not part of the game");
+		} catch (IllegalStateException expected) {
+			
+		}
+	}
+	
+	public void addBidToNonOpenGame() {
+		try {
+			spa.addBid(helper.getBid(flb, kimi, massa, hamilton, alonso, heidfeld, kubica, 1, 1, 100000));
+			fail("Game is not open");
+		} catch (IllegalStateException expected) {
+			
+		}
+	}
+
+	public void addBidToClosedGame() {
+		try {
+			Race australia = new Race();
+			australia.setSelectedDriver(massa);
+			australia.setName("Spa");
+			Calendar begin = Calendar.getInstance();
+			begin.add(Calendar.MINUTE, -7);
+			australia.setBegin(begin);
+			Calendar close2 = Calendar.getInstance();
+			close2.add(Calendar.MINUTE, -5);
+			australia.setClose(close2);
+			season.addRace(australia);
+			australia.addBid(helper.getBid(flb, kimi, massa, hamilton, alonso, heidfeld, kubica, 1, 1, 100000));
+			fail("Game is not open");
+		} catch (IllegalStateException expected) {
+			
+		}
+	}
+	
+	public void testDoubleAddBid() {
+		Bid bid = helper.getBid(flb, massa, kimi, hamilton, alonso, heidfeld, kubica, 1, 1, 100000);
+		monza.addBid(bid);
+		getEntityManager().flush();
+		assertNotNull(bid.getId());
+		try {
+			bid = helper.getBid(flb, massa, kimi, hamilton, heidfeld, alonso, kubica, 1, 1, 100000);
+			monza.addBid(bid);
+			fail("Double bid from same player should fail");
+		} catch (IllegalStateException expected) {
+			
+		}
+	}
+	
+	
+	
 	public void testValidDates() {
-		race.setBegin(Calendar.getInstance());
+		monza.setBegin(Calendar.getInstance());
 		Calendar close = Calendar.getInstance();
 		close.add(Calendar.MONTH, 1);
 		try {
-			race.setClose(close);
+			monza.setClose(close);
 		} catch (IllegalStateException e) {
 			fail("Dates should have been ok");
 		}
 	}
 	
 	public void testInvalidDates() {
-		race.setBegin(Calendar.getInstance());
-		race.getBegin().add(Calendar.MONTH, 1);
+		monza.setBegin(Calendar.getInstance());
+		monza.getBegin().add(Calendar.MONTH, 1);
 		Calendar close = Calendar.getInstance();
 		try {
-			race.setClose(close);
+			monza.setClose(close);
 			fail("Dates should have failed ok");
 		} catch (IllegalStateException expected) {
 		}
@@ -75,5 +125,70 @@ public class RaceTest extends AbstractDaoTest {
     public void testRaceConfiguration() {
         Race race = new Race();
         assertNotNull("Result strategy should have been provided", race.getResultStrategy());
+    }
+    
+    public void testRaceResultNonClosedGame() {
+    	
+    	try {
+    		monza.setRaceResult(raceResult);
+    	} catch (IllegalStateException expected) {
+    	}
+    }
+
+    public void testRaceResultGame() throws Exception {
+    	Thread.sleep(200);
+    	monza.setClose(Calendar.getInstance());
+    	Thread.sleep(200);
+   		monza.setRaceResult(raceResult);
+   		getEntityManager().flush();
+   		assertNotNull(raceResult.getId());
+    }
+
+    public void testResult() throws Exception{
+    	monza.addBid(flbBid);
+    	monza.addBid(mbaBid);
+    	monza.addBid(ttpBid);
+    	
+    	Thread.sleep(200);
+    	monza.setClose(Calendar.getInstance());
+    	Thread.sleep(200);
+   		monza.setRaceResult(raceResult);
+   		getEntityManager().flush();
+    	
+   		assertNotNull(flbBid.getId());
+   		assertNotNull(mbaBid.getId());
+   		assertNotNull(ttpBid.getId());
+ 		assertNotNull(raceResult.getId());
+ 		
+ 		List<Bid> result = monza.getResult();
+ 		assertEquals(3, result.size());
+ 		assertEquals(flbBid, result.get(0));
+ 		assertEquals(mbaBid, result.get(1));
+ 		assertEquals(ttpBid, result.get(2));
+    }
+
+    public void testResultBetterPole() throws Exception{
+    	monza.addBid(flbBid);
+    	mbaBid = helper.getBid(mba, kimi, massa, hamilton, alonso, heidfeld, kubica, 1, 1, 11000);
+		
+    	monza.addBid(mbaBid);
+    	monza.addBid(ttpBid);
+    	
+    	Thread.sleep(200);
+    	monza.setClose(Calendar.getInstance());
+    	Thread.sleep(200);
+   		monza.setRaceResult(raceResult);
+   		getEntityManager().flush();
+    	
+   		assertNotNull(flbBid.getId());
+   		assertNotNull(mbaBid.getId());
+   		assertNotNull(ttpBid.getId());
+ 		assertNotNull(raceResult.getId());
+ 		
+ 		List<Bid> result = monza.getResult();
+ 		assertEquals(3, result.size());
+ 		assertEquals(flbBid, result.get(0));
+ 		assertEquals(mbaBid, result.get(1));
+ 		assertEquals(ttpBid, result.get(2));
     }
 }
--- core/src/test/java/dk/bregnvig/formula1/util/CreatorHelper.java
+++ core/src/test/java/dk/bregnvig/formula1/util/CreatorHelper.java
@@ -2,9 +2,19 @@ package dk.bregnvig.formula1.util;
 
 import java.util.Calendar;
 
+import dk.bregnvig.formula1.Bid;
 import dk.bregnvig.formula1.Driver;
 import dk.bregnvig.formula1.Player;
 import dk.bregnvig.formula1.Race;
+import dk.bregnvig.formula1.RaceResult;
+import dk.bregnvig.formula1.bid.FastestLapBid;
+import dk.bregnvig.formula1.bid.FirstCrashBid;
+import dk.bregnvig.formula1.bid.FirstCrashResult;
+import dk.bregnvig.formula1.bid.GridBid;
+import dk.bregnvig.formula1.bid.GridResult;
+import dk.bregnvig.formula1.bid.PodiumBid;
+import dk.bregnvig.formula1.bid.PodiumResult;
+import dk.bregnvig.formula1.bid.SelectedDriverBid;
 
 public class CreatorHelper {
 
@@ -34,5 +44,98 @@ public class CreatorHelper {
 		close.add(Calendar.WEEK_OF_YEAR, 1);
 		race.setClose(close);
 		return race;
+	}
+	
+	public Bid getBid(Player player, Driver driver1, Driver driver2, Driver driver3, Driver driver4, Driver driver5, Driver driver6, int begin, int end, int polePositionTimeMillis) {
+		Bid bid = new Bid();
+		bid.setPlayer(player);
+		
+		bid.setFastestLap(getFastestLapBid(driver1));
+		bid.setFirstCrash(getFirstCrashBid(driver6));
+		bid.setGrid(getGridBid(driver1, driver2, driver3, driver4, driver5, driver6));
+		bid.setPodium(getPodiumBid(driver1, driver2, driver3));
+		bid.setSelectedDriver(getSelectedDriverBid(begin, end));
+		bid.setPolePositionTimeMillis(polePositionTimeMillis);
+		return bid;
+	}
+
+	public RaceResult getRaceResult(Player player, Driver driver1, Driver driver2, Driver driver3, Driver driver4, Driver driver5, Driver driver6, Driver driver7, int begin, int end, int polePositionTimeMillis) {
+		RaceResult bid = new RaceResult();
+		bid.setPlayer(player);
+		
+		bid.setFastestLap(getFastestLapBid(driver1));
+		bid.setFirstCrash(getFirstCrashResult(driver6, driver5, driver4));
+		bid.setGrid(getGridResult(driver1, driver2, driver3, driver4, driver5, driver6, driver7));
+		bid.setPodium(getPodiumResult(driver1, driver2, driver3, driver4));
+		bid.setSelectedDriver(getSelectedDriverBid(begin, end));
+		bid.setPolePositionTimeMillis(polePositionTimeMillis);
+		return bid;
+	}
+	
+	private PodiumResult getPodiumResult(Driver driver1, Driver driver2, Driver driver3, Driver driver4) {
+		PodiumResult result = new PodiumResult();
+		result.setPosition1(driver1);
+		result.setPosition2(driver2);
+		result.setPosition3(driver3);
+		result.setPosition4(driver4);
+		return result;
+	}
+
+	private GridResult getGridResult(Driver driver1, Driver driver2, Driver driver3, Driver driver4, Driver driver5, Driver driver6, Driver driver7) {
+		GridResult result = new GridResult();
+		result.setPosition1(driver1);
+		result.setPosition2(driver2);
+		result.setPosition3(driver3);
+		result.setPosition4(driver4);
+		result.setPosition5(driver5);
+		result.setPosition6(driver6);
+		result.setPosition7(driver7);
+		return result;	
+	}
+
+	private FirstCrashResult getFirstCrashResult(Driver driver1, Driver driver2, Driver driver3) {
+		FirstCrashResult result = new FirstCrashResult();
+		result.setCrash1(driver1);
+		result.setCrash2(driver2);
+		result.setCrash3(driver3);
+		return result;
+	}
+
+	private SelectedDriverBid getSelectedDriverBid(int begin, int end) {
+		SelectedDriverBid bid = new SelectedDriverBid();
+		bid.setStartPosition(begin);
+		bid.setEndPosition(end);
+		return bid;
+	}
+
+	private PodiumBid getPodiumBid(Driver driver1, Driver driver2, Driver driver3) {
+		PodiumBid bid = new PodiumBid();
+		bid.setPosition1(driver1);
+		bid.setPosition2(driver2);
+		bid.setPosition3(driver3);
+		return bid;
+	}
+
+	private GridBid getGridBid(Driver driver1, Driver driver2, Driver driver3, Driver driver4, Driver driver5, Driver driver6) {
+		GridBid bid = new GridBid();
+		bid.setPosition1(driver1);
+		bid.setPosition2(driver2);
+		bid.setPosition3(driver3);
+		bid.setPosition4(driver4);
+		bid.setPosition5(driver5);
+		bid.setPosition6(driver6);
+		return bid;
+	}
+
+	public FastestLapBid getFastestLapBid(Driver driver) {
+		FastestLapBid bid = new FastestLapBid();
+		bid.setDriver(driver);
+		return bid;
+	}
+	
+	public FirstCrashBid getFirstCrashBid(Driver firstCrash) {
+		FirstCrashBid bid = new FirstCrashBid();
+		bid.setCrash1(firstCrash);
+		return bid;
 	}
 }
