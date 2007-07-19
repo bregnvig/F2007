68498a374e7319af9cde6b1f9e826d311f92b2e6
Flemming Bregnvig <flemming@bregnvig.dk>
2007-07-19 21:16:24 +0000

    Completed the one week reminder
    git-svn-id: https://svn.iportalen.dk/svn-private/F2007/trunk@239 d9c49cb3-4128-44d0-8390-2fdd9ac6ca64

--- core/src/main/java/dk/bregnvig/formula1/event/EmailReminderService.java
+++
@@ -1,8 +0,0 @@
-package dk.bregnvig.formula1.event;
-
-public class EmailReminderService extends AbstractRaceTimer {
-	
-	public void invoke() {
-		System.err.println("Race " + getRace().getName());
-	}
-}
