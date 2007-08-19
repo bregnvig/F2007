6d7ef457b045b1730f8bee89cec96447a0bcdb7b
Flemming Bregnvig <flemming@bregnvig.dk>
2007-08-19 10:17:56 +0000

    *** empty log message ***
    git-svn-id: https://svn.iportalen.dk/svn-private/F2007/trunk@152 d9c49cb3-4128-44d0-8390-2fdd9ac6ca64

---
+++ core/src/main/java/dk/bregnvig/formula1/util/impl/SMSServiceImpl.java
@@ -0,0 +1,18 @@
+package dk.bregnvig.formula1.util.impl;
+
+import dk.bregnvig.formula1.util.SMSService;
+
+/**
+ * This class handles the sending of the SMS for real. Will only work if the service is available
+ * $Revision: 1.1 $
+ * $Date: 2007-08-19 10:17:56 $
+ * @author 4u603ctpsz9
+ */
+public class SMSServiceImpl implements SMSService {
+
+	public void sendSMS(String phoneNumber, String message) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
