840827939977cca89ce956f033174c79ecedfed4
Flemming Bregnvig <flemming@bregnvig.dk>
2007-10-23 19:25:09 +0000

    Latest release
    git-svn-id: https://svn.iportalen.dk/svn-private/F2007/trunk@171 d9c49cb3-4128-44d0-8390-2fdd9ac6ca64

--- core/src/test/java/dk/bregnvig/formula1/util/SpringStarter.java
+++ core/src/test/java/dk/bregnvig/formula1/util/SpringStarter.java
@@ -17,7 +17,7 @@ public class SpringStarter {
 	static {
 		configLocations.add("classpath:contexts/data/dataLayerContext.xml");
 		configLocations.add("classpath:contexts/data/daoContext.xml");
-		configLocations.add("classpath:contexts/data/daoITestContext.xml");
+		configLocations.add("classpath:contexts/data/testDataLayerContext.xml");
 		configLocations.add("classpath:contexts/service/serviceContext.xml");
 		configLocations.add("classpath:contexts/service/serviceTestContext.xml");
 	};
