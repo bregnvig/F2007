b7f1759783ac946b109d03356e34510b9bedfa09
Flemming Bregnvig <flemming@bregnvig.dk>
2010-03-07 11:39:01 +0000

    Added flash to version
    git-svn-id: https://svn.iportalen.dk/svn-private/F2007/trunk@347 d9c49cb3-4128-44d0-8390-2fdd9ac6ca64

---
+++ web/F2007-compile.cmd
@@ -0,0 +1,1 @@
+java -Xmx768m -cp ".\src\main\java;.\src\main\resources;.\target\classes;C:\Java\gwt-windows-1.5.3\gwt-user.jar;C:\Java\gwt-windows-1.5.3\gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out ".\target\www" dk.bregnvig.formula1.F2007
