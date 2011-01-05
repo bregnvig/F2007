bd2ff1ece136d3bcd3371b06b5bd14cccceae232
Flemming Bregnvig <flemming@bregnvig.dk>
2011-01-05 15:11:36 +0000

    Added restful stuff
    git-svn-id: https://svn.iportalen.dk/svn-private/F2007/trunk@361 d9c49cb3-4128-44d0-8390-2fdd9ac6ca64

---
+++ web/src/main/java/dk/bregnvig/formula1/server/restful/GameRestfulController.java
@@ -0,0 +1,34 @@
+package dk.bregnvig.formula1.server.restful;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import dk.bregnvig.formula1.client.domain.ClientPlayer;
+import dk.bregnvig.formula1.client.exception.CredentialException;
+import dk.bregnvig.formula1.client.service.GameService;
+
+@Controller
+public class GameRestfulController {
+	
+	@Autowired private GameService service;
+ 	@Autowired public HttpServletRequest request;
+ 	
+	@RequestMapping("/login/{playerName}/{password}")
+	public ClientPlayer login(@PathVariable String playerName, @PathVariable String password) throws CredentialException {
+		System.out.println(playerName + ":" + password + ":" + request);
+		return service.login(playerName, password);
+	}
+	
+	@ExceptionHandler(CredentialException.class)
+	public void handleCredentialException(CredentialException e, HttpServletResponse response) throws IOException {
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+	}
+}
